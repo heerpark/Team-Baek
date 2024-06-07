216a032b3afe5c6acd9521f641446c156a783333
seoyeon2001 <syeon1250@naver.com>
2024-06-07 09:44:51 +0900

    FIX: dataset & error

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ from streamlit_text_rating.st_text_rater import st_text_rater # pip install stre
 from streamlit_chat import message # pip install streamlit-chat
 from PIL import Image
 
+# 데이터 읽어오기
 def read_data(file):
     raw = pd.read_csv(file)
     return raw
 
-rating = read_data('data/naver_and_kakao_star.csv')
-kakao_map_review = read_data('data/kakao_restaurant_5.csv')
-naver_map_review = read_data('data/naver_restaurant_5.csv')
-kakao_map_review=kakao_map_review.reset_index()
-naver_map_review=naver_map_review.reset_index()
+# 이미지 크기 조정
+def resize_image(image, size):
+    return image.resize(size)
 
+# 사이드바 필터
 def sidebar_filters():
     st.title("평점 범위를 선택하세요.")
     st.write("평점을 선택하면 해당하는 음식점을 추천해드립니다.")
@@ -38,12 +38,9 @@ def sidebar_filters():
         else:
             filtered_df = st.session_state.filtered_df
 
-        if "selected_restaurant" not in st.session_state:
-            st.session_state.selected_restaurant = ''
-
         if len(filtered_df) > 0:
             restaurant_list = filtered_df['음식점'].tolist()
-            selected_restaurant = st.selectbox("필터링된 음식점을 선택하세요", restaurant_list)
+            selected_restaurant = st.selectbox("필터링된 음식점 중 보고싶은 가게를 선택하세요", restaurant_list, index=None, placeholder="음식점",)
             st.session_state.selected_restaurant = selected_restaurant
             st.write(f"선택한 음식점: {st.session_state.selected_restaurant}")
         else:
@@ -51,9 +48,7 @@ def sidebar_filters():
     else:
         st.write("조건을 설정하고 '적용하기'를 눌러주세요.")
 
-# 가게 이름
-restaurant_name = "하이레\n\n"
-
+# 감성 점수 시각화
 def show_sentimental_score(category, naver_score, kakao_score, blog_score):
     progress_text1 = f"네이버 리뷰에서 분석한 {category}에 대한 감성 점수에요"
     progress_text2 = f"카카오 리뷰에서 {category}에 대한 분석한 감성 점수에요"
@@ -66,6 +61,7 @@ def show_sentimental_score(category, naver_score, kakao_score, blog_score):
     my_bar1.empty()
     my_bar2.empty()
     my_bar3.empty()
+
     for percent_complete in range(100):
         time.sleep(0.005)
         if (percent_complete < naver_score):
@@ -75,108 +71,96 @@ def show_sentimental_score(category, naver_score, kakao_score, blog_score):
         if (percent_complete < blog_score):
             my_bar3.progress(percent_complete + 1, text=progress_text3)
 
-def resize_image(image, size):
-    return image.resize(size)
-
+# 데이터 읽어오기
+rating = read_data('data/naver_and_kakao_star.csv')
+kakao_map_review = read_data('data/kakao_restaurant_5.csv').reset_index()
+naver_map_review = read_data('data/naver_restaurant_5.csv').reset_index()
 
 
 def main():
     with st.sidebar:
         sidebar_filters()
 
-    restaurant_name='미선택'
-
-    # if "selected_restaurant" in st.session_state and st.session_state.selected_restaurant:
-    #     restaurant_data = rating[rating["음식점"] == st.session_state.selected_restaurant].iloc[0]
-    #     #show_restaurant_info(restaurant_data)
-    #     st.divider()
-    #     st_text_rater(text=f"{st.session_state.selected_restaurant}를 평가해주세요")
-    # else:
-    #     st.title("음식점을 선택해주세요")
-    #     st.write("사이드바에서 조건을 설정하고 '적용하기' 버튼을 눌러 음식점을 선택하세요.")
-
-    #sidebar에서 선택한 음식점이름 가져오기
-
     if "selected_restaurant" not in st.session_state:
         st.session_state.selected_restaurant = ''
 
     if st.session_state.selected_restaurant:
         restaurant_name = st.session_state.selected_restaurant
-    else:
-        st.title("아직 선택되지 않았습니다.")
 
-    # 페이지 제목
-    st.title("🍽️"+restaurant_name)
-    st.write("")
+        # 페이지 제목
+        st.title("🍽️"+restaurant_name)
+        st.write("")
 
-    path='img/'+restaurant_name
+        path='img/'+restaurant_name 
 
-    # 이미지 열기
-    image1 = Image.open(path+'/img1.jpg')
-    image2 = Image.open(path+'/img2.jpg')
+        try: 
+            # 이미지 열기
+            image1 = Image.open(path+'/img1.jpg')
+            image2 = Image.open(path+'/img2.jpg')
 
-    # 이미지를 동일한 크기로 조정
-    target_size = (300, 300)
-    image1_resized = resize_image(image1, target_size)
-    image2_resized = resize_image(image2, target_size)
+            # 이미지를 동일한 크기로 조정
+            target_size = (300, 300)
+            image1_resized = resize_image(image1, target_size)
+            image2_resized = resize_image(image2, target_size)
 
-    col1, col2 = st.columns(2)
-    with col1:
-        st.image(image1_resized, use_column_width=True)
-    with col2:
-        st.image(image2_resized, use_column_width=True)
-    st.write("")
+            col1, col2 = st.columns(2)
+            with col1:
+                st.image(image1_resized, use_column_width=True)
+            with col2:
+                st.image(image2_resized, use_column_width=True)
+            st.write("")
 
-    # Ai 분석결과 시각화
-    tabs = st.tabs(["😋맛", "🙋서비스", "🧹청결", "🌌분위기"])
+            # Ai 분석결과 시각화
+            tabs = st.tabs(["😋맛", "🙋서비스", "🧹청결", "🌌분위기"])
 
-    with tabs[0]:
-        show_sentimental_score('맛', 80, 90, 100)
-        container = st.container(border=True, height=200)
-        container.write("🤖인공지능이 요약한 맛에 대한 리뷰에요🤖\n\n하이레의 돈까스는 진짜 너무맛있어요. 특히 안심은 진짜 넘사입니다.")
+            with tabs[0]:
+                show_sentimental_score('맛', 80, 90, 100)
+                container = st.container(border=True, height=200)
+                container.write("🤖인공지능이 요약한 맛에 대한 리뷰에요🤖\n\n하이레의 돈까스는 진짜 너무맛있어요. 특히 안심은 진짜 넘사입니다.")
 
-    with tabs[1]:
-        show_sentimental_score('서비스', 30, 10, 50)
-        container = st.container(border=True, height=200)
-        container.write("🤖인공지능이 요약한 서비스에 대한 리뷰에요🤖\n\n하이레는 사장님들 서비스가 장난이 아니에요")
+            with tabs[1]:
+                show_sentimental_score('서비스', 30, 10, 50)
+                container = st.container(border=True, height=200)
+                container.write("🤖인공지능이 요약한 서비스에 대한 리뷰에요🤖\n\n하이레는 사장님들 서비스가 장난이 아니에요")
 
-    with tabs[2]:
-        show_sentimental_score('청결', 80, 90, 80)
-        container = st.container(border=True, height=200)
-        container.write("🤖인공지능이 요약한 청결에 대한 리뷰에요🤖\n\n하이레 사장님들이 맨날 청소하더라고요 !! 믿고 갑니다.")
+            with tabs[2]:
+                show_sentimental_score('청결', 80, 90, 80)
+                container = st.container(border=True, height=200)
+                container.write("🤖인공지능이 요약한 청결에 대한 리뷰에요🤖\n\n하이레 사장님들이 맨날 청소하더라고요 !! 믿고 갑니다.")
 
-    with tabs[3]:
-        show_sentimental_score('분위기', 100, 90, 100)
-        container = st.container(border=True, height=200)
-        container.write("🤖인공지능이 요약한 분위기에 대한 리뷰에요🤖\n\n하이레 들어온 순간 여기는 광운대가 아니라 오사카")
-    st.write("")
+            with tabs[3]:
+                show_sentimental_score('분위기', 100, 90, 100)
+                container = st.container(border=True, height=200)
+                container.write("🤖인공지능이 요약한 분위기에 대한 리뷰에요🤖\n\n하이레 들어온 순간 여기는 광운대가 아니라 오사카")
+            st.write("")
 
-    kakao = kakao_map_review[kakao_map_review['음식점'] == restaurant_name]
-    kakao=kakao.reset_index()
-    naver = naver_map_review[naver_map_review['음식점'] == restaurant_name]
-    naver=naver.reset_index()
+            kakao = kakao_map_review[kakao_map_review['음식점'] == restaurant_name].reset_index()
+            naver = naver_map_review[naver_map_review['음식점'] == restaurant_name].reset_index()
 
-    tab1, tab2, tab3 = st.tabs(["1~4", "4~8", "8~12"])
+            tab1, tab2, tab3 = st.tabs(["1~4", "4~8", "8~12"])
 
-    with tab1:
-        for i in range(0, 4):
-            message("naver\n" + naver['리뷰'][i])
-            message("kakao\n" + kakao['리뷰'][i], is_user=True)
+            with tab1:
+                for i in range(0, 4):
+                    message("naver\n" + naver['리뷰'][i])
+                    message("kakao\n" + kakao['리뷰'][i], is_user=True)
 
-    with tab2:
-        for i in range(4, 8):
-            message("naver\n" + naver['리뷰'][i])
-            message("kakao\n" + kakao['리뷰'][i], is_user=True)
+            with tab2:
+                for i in range(4, 8):
+                    message("naver\n" + naver['리뷰'][i])
+                    message("kakao\n" + kakao['리뷰'][i], is_user=True)
 
-    with tab3:
-        for i in range(8, 12):
-            message("naver\n" + naver['리뷰'][i])
-            message("kakao\n" + kakao['리뷰'][i], is_user=True)
+            with tab3:
+                for i in range(8, 12):
+                    message("naver\n" + naver['리뷰'][i])
+                    message("kakao\n" + kakao['리뷰'][i], is_user=True)
 
-    st.divider()
+            st.divider()
 
-    # 음식집 평가
-    st_text_rater(text=f"{restaurant_name}를 평가해주세요")
+            # 음식집 평가
+            st_text_rater(text=f"{restaurant_name}를 평가해주세요")
+
+        except FileNotFoundError:
+            pass
 
 if __name__ == "__main__":
     main()
